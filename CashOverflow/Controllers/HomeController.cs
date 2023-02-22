bd944c0d852749dbd8501fabf00fa47e5081c99b
Elbek <e.normurodov@mail.ru>
2023-02-22 08:15:34 -0800

    Add project files.

---
+++ CashOverflow/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashOverflow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Cash flows...";
+    }
+}
---
+++ CashOverflow/Program.cs
@@ -0,0 +1,25 @@
+// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace CashOverflow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
