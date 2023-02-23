eb0b48c7405150484ddbd9920f9c6fe0d2441f6c
Elbek <e.normurodov@mail.ru>
2023-02-23 08:01:24 -0800

    INFRA: Create Build Project

---
+++ CashOverlow.Infrastructure.Build/Program.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace CashOverlow.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Build & Test CashOverlow",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "master" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "master" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Checking out"
+                            }, 
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name= "Installing .NET",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "7.0.200"
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restoring packages"
+                            }, 
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Building project"
+                            }, 
+
+                            new TestTask
+                            {
+                                Name = "Running tests"
+                            }
+                        }
+                    }
+                }
+            };
+
+
+            var adotnetClient = new ADotNetClient();
+
+            adotnetClient.SerializeAndWriteToFile(
+                githubPipeline,
+                path: "../../../../.github/workflows/build.yml");
+        }
+    }
+}
