6d750ac95bca7616aca67f19333287baa3448b4d
Ulugbek <ulugbektoshtemirov30052000@gmail.com>
2023-02-27 01:42:19 +0500

    DATA: Model & Migrate Salaries

---
+++ CashOverflow/Brokers/Storages/StorageBroker.Salary.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+using CashOverflow.Models.Locations;
+using CashOverflow.Models.Salaries;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashOverflow.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Salary> Salaries { get; set; }
+    }
+}
