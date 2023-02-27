0f8892db226e7f910f11ee4a03726255330cdfe9
jahon1507 <User@USER-PC>
2023-02-27 22:08:45 +0500

    DATA : Model & Migrate 'Language'

---
+++ CashOverflow/Models/Languages/Type.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+namespace CashOverflow.Models.Languages
+{
+    public enum Type
+    {
+        Other,
+        Frontend,
+        Backend,
+        Fullstack
+    }
+}
