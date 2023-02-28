7fc3430912b749cefcdec5371ec1b9805339b124
mohinur18 <muhammadjonovamohinur36@gmail.com>
2023-02-28 21:04:14 +0500

    BROKERS : Insert Jobs

--- CashOverflow/Brokers/Storages/StorageBroker.Jobs.cs
+++ CashOverflow/Brokers/Storages/StorageBroker.Jobs.cs
@@ -5,11 +5,15 @@
 
 using CashOverflow.Models.Jobs;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace CashOverflow.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Job> Jobs { get; set; }
+
+        public async ValueTask<Job> InsertJobAsync(Job job) =>
+            await InsertAsync(job);
     }
 }
