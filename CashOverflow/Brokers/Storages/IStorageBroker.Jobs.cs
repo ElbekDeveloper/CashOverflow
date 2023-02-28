523639cc70c396bcf43252ebe9554bc8bd1a04f6
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-02-28 22:08:14 +0500

    CODE RUB: Fix Coding

--- CashOverflow/Brokers/Storages/IStorageBroker.Jobs.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Jobs.cs
@@ -1,6 +1,15 @@
-﻿namespace CashOverflow.Brokers.Storages
+﻿using System.Linq;
+// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Models.Jobs;
+
+namespace CashOverflow.Brokers.Storages
 {
-    public class IStorageBroker
+    public partial interface IStorageBroker
     {
+        IQueryable<Job> SelectAllJobs();
     }
 }
