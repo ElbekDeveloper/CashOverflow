b211fb27fb10550134874f28de20f404d3898a66
mohinur18 <muhammadjonovamohinur36@gmail.com>
2023-02-28 21:33:42 +0500

    CODE RUB : Rewrite Insert Method

--- CashOverflow/Brokers/Storages/IStorageBroker.Jobs.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Jobs.cs
@@ -1,6 +1,15 @@
-﻿namespace CashOverflow.Brokers.Storages
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Models.Jobs;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Brokers.Storages
 {
-    public class IStorageBroker
+    public partial interface IStorageBroker
     {
+        ValueTask<Job> InsertJobAsync(Job job);
     }
 }
