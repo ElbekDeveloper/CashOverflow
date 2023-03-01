94cb3729f4024ec2c796c0fceb4ece5d8a2a0750
Haydarovcoder <abboshaydarov0799@gmail.com>
2023-03-01 11:31:00 +0500

    CODE RUB: Write SelectLanguageById Method

--- CashOverflow/Brokers/Storages/IStorageBroker.Languages.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Languages.cs
@@ -3,6 +3,7 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using CashOverflow.Models.Languages;
 
@@ -11,6 +12,7 @@ namespace CashOverflow.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Language> InsertLanguageAsync(Language language);
+        ValueTask<Language> SelectLanguageByIdAsync(Guid languageId);
         ValueTask<Language> UpdateLanguageAsync(Language language);
         ValueTask<Language> DeleteLanguageAsync(Language language);
     }
