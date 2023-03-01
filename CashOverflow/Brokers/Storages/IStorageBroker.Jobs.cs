1529dd1cf2a0869dc57aabb632b84a5340e0f86e
KhusanRahmatullayev <125781453+KhusanRahmatullayev@users.noreply.github.com>
2023-03-01 09:53:02 +0500

    BROKERS:Select Job By Id

--- CashOverflow/Brokers/Storages/IStorageBroker.Jobs.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Jobs.cs
@@ -4,6 +4,7 @@
 // --------------------------------------------------------
 
 using CashOverflow.Models.Jobs;
+using System;
 using System.Threading.Tasks;
 
 namespace CashOverflow.Brokers.Storages
@@ -11,5 +12,7 @@ namespace CashOverflow.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Job> InsertJobAsync(Job job);
+
+        ValueTask<Job> SelectJobByIdAsync(Guid jobId);   
     }
 }
--- CashOverflow/Brokers/Storages/StorageBroker.Jobs.cs
+++ CashOverflow/Brokers/Storages/StorageBroker.Jobs.cs
@@ -5,6 +5,7 @@
 
 using CashOverflow.Models.Jobs;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
 
 namespace CashOverflow.Brokers.Storages
@@ -15,5 +16,8 @@ namespace CashOverflow.Brokers.Storages
 
         public async ValueTask<Job> InsertJobAsync(Job job) =>
             await InsertAsync(job);
+
+        public async ValueTask<Job> SelectJobById(Guid jobId) =>
+            await SelectAsync<Job>(jobId);
     }
 }
