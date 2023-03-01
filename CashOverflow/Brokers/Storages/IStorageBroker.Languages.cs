7986941e78bcec8c4c49b581b1d57215acf66f51
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-01 15:02:33 +0500

    CODE RUB: Fix The Problem

--- CashOverflow/Brokers/Storages/IStorageBroker.Languages.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Languages.cs
@@ -7,14 +7,13 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using CashOverflow.Models.Languages;
-using CashOverflow.Models.Locations;
 
 namespace CashOverflow.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
         ValueTask<Language> InsertLanguageAsync(Language language);
-        IQueryable<Location> SelectAllLanguages();
+        IQueryable<Language> SelectAllLanguages();
         ValueTask<Language> SelectLanguageByIdAsync(Guid languageId);
         ValueTask<Language> UpdateLanguageAsync(Language language);
         ValueTask<Language> DeleteLanguageAsync(Language language);
