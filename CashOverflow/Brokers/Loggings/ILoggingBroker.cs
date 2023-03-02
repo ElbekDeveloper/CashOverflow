51362a10c2c3992acb069db0b0acaf5227512021
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-02 20:31:20 +0500

    BROKERS: Add Logging

---
+++ CashOverflow/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+
+namespace CashOverflow.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
