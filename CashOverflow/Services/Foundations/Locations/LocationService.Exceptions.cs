b77164e1cfe39f84d4a16ec022daa623f3da4765
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-02 21:04:25 +0500

    CODE RUB: Refactor Method

---
+++ CashOverflow/Services/Foundations/Locations/LocationService.Exceptions.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using CashOverflow.Models.Locations;
+using CashOverflow.Models.Locations.Exceptions;
+using Xeptions;
+
+namespace CashOverflow.Services.Foundations.Locations
+{
+    public partial class LocationService
+    {
+        private delegate ValueTask<Location> ReturningLocationFunction();
+
+        private async ValueTask<Location> TryCatch(ReturningLocationFunction returningLocationFunction)
+        {
+            try
+            {
+                return await returningLocationFunction();
+            }
+            catch (NullLocationException nullLocationException)
+            {
+                throw CreateAndLogValidationException(nullLocationException);
+            }
+        }
+
+        private LocationValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var locationValidationException = new LocationValidationException(exception);
+            this.loggingBroker.LogError(locationValidationException);
+
+            return locationValidationException;
+        }
+    }
+}
--- CashOverflow/Services/Foundations/Locations/LocationService.cs
+++ CashOverflow/Services/Foundations/Locations/LocationService.cs
@@ -11,7 +11,7 @@ using CashOverflow.Models.Locations.Exceptions;
 
 namespace CashOverflow.Services.Foundations.Locations
 {
-    public class LocationService : ILocationService
+    public partial class LocationService : ILocationService
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
@@ -24,24 +24,12 @@ namespace CashOverflow.Services.Foundations.Locations
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<Location> AddLocationAsync(Location location)
+        public ValueTask<Location> AddLocationAsync(Location location) =>
+        TryCatch(async () =>
         {
-            try
-            {
-                if (location is null)
-                {
-                    throw new NullLocationException();
-                }
+            ValidateLocationNotNull(location);
 
-                return await this.storageBroker.InsertLocationAsync(location);
-            }
-            catch (NullLocationException nullLocationException)
-            {
-                var locationValidationException = new LocationValidationException(nullLocationException);
-                this.loggingBroker.LogError(locationValidationException);
-
-                throw locationValidationException;
-            }
-        }
+            return await this.storageBroker.InsertLocationAsync(location);
+        });
     }
 }
