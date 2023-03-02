2a7bbf48612be97c620f0bc979dfab1ab532b6c4
MirzayevDeveloper <MirzayevDeveloper1@gamil.com>
2023-03-02 16:11:38 +0500

    ShouldAddLanguageAsync -> FAIL

---
+++ CashOverflow/Services/Foundations/Languages/ILanguageService.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Models.Languages;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Services.Foundations.Languages
+{
+    public interface ILanguageService
+    {
+        ValueTask<Language> AddLanguageAsync(Language language);
+    }
+}
