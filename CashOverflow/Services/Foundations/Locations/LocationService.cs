ca4673f251f92d21cdd0b2ff2c8a642a8f63d7a4
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-02 20:55:33 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

--- CashOverflow/Services/Foundations/Locations/LocationService.cs
+++ CashOverflow/Services/Foundations/Locations/LocationService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using CashOverflow.Brokers.Loggings;
 using CashOverflow.Brokers.Storages;
 using CashOverflow.Models.Locations;
+using CashOverflow.Models.Locations.Exceptions;
 
 namespace CashOverflow.Services.Foundations.Locations
 {
@@ -16,14 +17,31 @@ namespace CashOverflow.Services.Foundations.Locations
         private readonly ILoggingBroker loggingBroker;
 
         public LocationService(
-            IStorageBroker storageBroker, 
+            IStorageBroker storageBroker,
             ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<Location> AddLocationAsync(Location location) =>
-            await this.storageBroker.InsertLocationAsync(location);
+        public async ValueTask<Location> AddLocationAsync(Location location)
+        {
+            try
+            {
+                if (location is null)
+                {
+                    throw new NullLocationException();
+                }
+
+                return await this.storageBroker.InsertLocationAsync(location);
+            }
+            catch (NullLocationException nullLocationException)
+            {
+                var locationValidationException = new LocationValidationException(nullLocationException);
+                this.loggingBroker.LogError(locationValidationException);
+
+                throw locationValidationException;
+            }
+        }
     }
 }
