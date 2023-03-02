56ade4a6f1dde6b288b17183a776e4097164d363
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-02 20:27:47 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> WIP

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Validations.Add.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Locations
+{
+    public partial class LocationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+}
