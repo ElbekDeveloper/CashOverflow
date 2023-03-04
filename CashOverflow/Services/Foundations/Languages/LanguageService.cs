8f4a59d61429f51d6bda9ddbdadd4dbc03d1ac8d
MirzayevDeveloper <MirzayevDeveloper1@gamil.com>
2023-03-04 17:49:15 +0500

    ShouldAddLanguageAsync -> PASS

--- CashOverflow/Services/Foundations/Languages/LanguageService.cs
+++ CashOverflow/Services/Foundations/Languages/LanguageService.cs
@@ -18,6 +18,6 @@ namespace CashOverflow.Services.Foundations.Languages
             this.storageBroker = storageBroker;
 
         public ValueTask<Language> AddLanguageAsync(Language language) =>
-            throw new NotImplementedException();
+            this.storageBroker.InsertLanguageAsync(language);
     }
 }
