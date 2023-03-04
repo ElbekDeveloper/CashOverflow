5b9119d0deb54d2f6d85095b4c6cbe40a657105b
MirzayevDeveloper <MirzayevDeveloper1@gamil.com>
2023-03-04 06:20:35 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNull -> WIP

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Validations.Add.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNull()
+        {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+}
