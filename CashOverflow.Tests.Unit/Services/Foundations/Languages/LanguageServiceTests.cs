46c949958c711dfb96ba5886456f33cc4493858c
MirzayevDeveloper <MirzayevDeveloper1@gamil.com>
2023-03-04 17:48:27 +0500

    ShouldAddLanguageAsync -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using CashOverflow.Brokers.Storages;
+using CashOverflow.Models.Languages;
+using CashOverflow.Services.Foundations.Languages;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly ILanguageService languageService;
+
+        public LanguageServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.languageService = new LanguageService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private DateTimeOffset GetRandomDatetimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private Language CreateRandomLanguage() =>
+            CreateLanguageFiller(GetRandomDatetimeOffset()).Create();
+
+        private Filler<Language> CreateLanguageFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Language>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
---
+++ CashOverflow/Services/Foundations/Languages/LanguageService.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CashOverflow.Brokers.Storages;
+using CashOverflow.Models.Languages;
+
+namespace CashOverflow.Services.Foundations.Languages
+{
+    public class LanguageService : ILanguageService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public LanguageService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public ValueTask<Language> AddLanguageAsync(Language language) =>
+            throw new NotImplementedException();
+    }
+}
