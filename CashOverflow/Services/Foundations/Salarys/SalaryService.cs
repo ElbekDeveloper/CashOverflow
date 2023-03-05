1dff5717657025c96727a1f5a9c1a871f949317f
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-03-05 16:15:12 +0500

    SERVICES: Created Add Salary

---
+++ CashOverflow/Services/Foundations/Salarys/SalaryService.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Brokers.Storages;
+using CashOverflow.Models.Salaries;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Services.Foundations.Salarys
+{
+    public class SalaryService : ISalaryService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public SalaryService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<Salary> AddSalaryAsync(Salary salary) =>
+            return await storageBroker.AddSalaryAsync(salary);
+    }
+}
