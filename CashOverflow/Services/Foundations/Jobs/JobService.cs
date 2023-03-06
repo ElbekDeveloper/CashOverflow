476b543b70bf84ad32fb592e3bf257bd6efeb62e
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-06 14:28:49 +0500

    ShouldRetrieveAllJobs -> PASS

--- CashOverflow/Services/Foundations/Jobs/JobService.cs
+++ CashOverflow/Services/Foundations/Jobs/JobService.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CashOverflow.Brokers.Storages;
+using CashOverflow.Models.Jobs;
 
 namespace CashOverflow.Services.Foundations.Jobs
 {
-    public class JobService
+    public class JobService: IJobService
     {
-        
+        private readonly IStorageBroker storageBroker;
+        public JobService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public IQueryable<Job> RetrieveAllJobs() =>
+            this.storageBroker.SelectAllJobs();
     }
 }
