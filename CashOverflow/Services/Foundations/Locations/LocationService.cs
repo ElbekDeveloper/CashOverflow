5530263cfe94bd0571ab4f506b9a441f66017829
jasons_xurshid <abdxurshid01@mail.ru>
2023-03-06 18:47:24 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

--- CashOverflow/Services/Foundations/Locations/LocationService.cs
+++ CashOverflow/Services/Foundations/Locations/LocationService.cs
@@ -4,21 +4,46 @@
 // --------------------------------------------------------
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using CashOverflow.Brokers.Loggings;
 using CashOverflow.Brokers.Storages;
 using CashOverflow.Models.Locations;
+using CashOverflow.Models.Locations.Exceptions;
 
 namespace CashOverflow.Services.Foundations.Locations
 {
-	public class LocationService:ILocationService
+    public class LocationService : ILocationService
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
-        public LocationService(IStorageBroker storageBroker)=>
-            this.storageBroker = storageBroker;
 
-        public async ValueTask<Location> AddLocationAsync(Location location) =>
-            await this.storageBroker.InsertLocationAsync(location);
+        public LocationService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+ 
+
+        public async ValueTask<Location> AddLocationAsync(Location location)
+        {
+            try
+            {
+                if (location is null)
+                {
+                    throw new NullLocationException(); 
+                }
+
+                return await this.storageBroker.InsertLocationAsync(location);
+            }
+            catch (NullLocationException nullLocationException)
+            {
+                var locationValidationException = new LocationValidationException(nullLocationException);
+                this.loggingBroker.LogError(locationValidationException);
+
+                throw locationValidationException;
+            }
+           
+        }
     }
 }
