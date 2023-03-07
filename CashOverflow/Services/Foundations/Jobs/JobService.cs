65d1f621568df03b1350ffdda9d1930f9fc6baef
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-07 23:27:45 +0500

    FOUNDATIONS: Dependency ILogging

--- CashOverflow/Services/Foundations/Jobs/JobService.cs
+++ CashOverflow/Services/Foundations/Jobs/JobService.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CashOverflow.Brokers.Loggings;
 using CashOverflow.Brokers.Storages;
 using CashOverflow.Models.Jobs;
 
@@ -14,6 +15,7 @@ namespace CashOverflow.Services.Foundations.Jobs
     public class JobService: IJobService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
         public JobService(IStorageBroker storageBroker)
         {
             this.storageBroker = storageBroker;
