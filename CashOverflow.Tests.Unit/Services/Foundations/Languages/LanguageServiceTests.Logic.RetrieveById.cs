f96ebb02296a93f4bc6b204c587a9f1e4e1d823a
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-03-08 22:19:00 +0500

    CODE RUB: Clean up

--- CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Logic.RetrieveById.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Logic.RetrieveById.cs
@@ -4,10 +4,6 @@
 // --------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CashOverflow.Models.Languages;
 using FluentAssertions;
 using Force.DeepCloner;
@@ -26,19 +22,17 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
             Guid inputLanguageId = randomLanguageId;
             Language randomLanguage = CreateRandomLanguage();
             Language storageLanguage = randomLanguage;
-            Language expectedLanguage = storageLanguage.DeepClone();
+            Language expectedLanguage = storageLanguage.DeepClone(); 
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectLanguageByIdAsync(inputLanguageId)).ReturnsAsync(storageLanguage);
             // when
             Language actualLanguage = await this.languageService.RetrieveByIdLanguages(inputLanguageId);
-
-
             // then
             actualLanguage.Should().BeEquivalentTo(expectedLanguage);
 
-            this.storageBrokerMock.Verify(broker => 
-                broker.SelectLanguageByIdAsync(inputLanguageId),Times.Once);
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectLanguageByIdAsync(inputLanguageId), Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
