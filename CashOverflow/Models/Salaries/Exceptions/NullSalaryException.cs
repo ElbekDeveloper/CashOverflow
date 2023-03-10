a0a35acc8daadeb8067437afff3241ff9cafdf81
KholmatovKhabibulla <85988278+khabibulladev@users.noreply.github.com>
2023-03-10 21:03:45 +0500

    CODE RUB: Move Exceptions Folder

---
+++ CashOverflow/Models/Salaries/Exceptions/NullSalaryException.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Xeptions;
+
+namespace CashOverflow.Models.Salaries.Exceptions
+{
+    public class NullSalaryException : Xeption
+    {
+
+        public NullSalaryException()
+            : base(message: "Salary is null.")
+        { }
+    }
+}
