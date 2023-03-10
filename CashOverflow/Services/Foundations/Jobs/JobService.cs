9bab3c9371818838088fe0fe174cae21e0f4d892
KhusanRahmatullayev <125781453+KhusanRahmatullayev@users.noreply.github.com>
2023-03-10 08:30:35 +0500

    ShouldRetrieveJobByIDAsync ->FAIL

---
+++ CashOverflow/Services/Foundations/Jobs/JobService.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CashOverflow.Brokers.DateTimes;
+using CashOverflow.Brokers.Loggings;
+using CashOverflow.Brokers.Storages;
+using CashOverflow.Models.Jobs;
+
+namespace CashOverflow.Services.Foundations.Jobs
+{
+    public class JobService : IJobService
+    {
+
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public JobService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+        public ValueTask<Job> RetrieveJobByIdAsync(Guid jobId) =>
+            throw new NotImplementedException();
+
+    }
+}
