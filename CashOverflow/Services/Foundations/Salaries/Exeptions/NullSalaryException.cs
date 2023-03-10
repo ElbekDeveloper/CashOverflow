9908d189e54ddab518eea4235b4fd5a794de261c
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-03-10 17:12:50 +0500

    SERVICE: Salary null exception and validation exception created

---
+++ CashOverflow/Services/Foundations/Salaries/Exeptions/NullSalaryException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace CashOverflow.Services.Foundations.Salaries.Exeptions
+{
+    public class NullSalaryException : Xeption
+    {
+
+        public NullSalaryException()
+            : base(message: "Salary is null.")
+        {  }
+    }
+}
---
+++ CashOverflow/Services/Foundations/Salaries/Exeptions/SalaryValidationException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace CashOverflow.Services.Foundations.Salaries.Exeptions
+{
+    public class SalaryValidationException : Xeption
+    {
+        public SalaryValidationException(Xeption innerException)
+            : base(message: "Salary validation error occurred, fix the error, and try again!", innerException)
+        { }
+    }
+}
