fd59c0be14329ceb978ae2b2c22d23dbcc30d302
KholmatovKhabibulla <85988278+khabibulladev@users.noreply.github.com>
2023-03-12 20:04:13 +0500

    CODE RUB: Refactor Method

---
+++ CashOverflow/Services/Foundations/Salaries/SalaryService.Exceptions.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using CashOverflow.Models.Salaries;
+using CashOverflow.Models.Salaries.Exceptions;
+using Xeptions;
+
+namespace CashOverflow.Services.Foundations.Salaries
+{
+    public partial class SalaryService
+    {
+        private delegate ValueTask<Salary> ReturningSalaryFucnction();
+
+        private async ValueTask<Salary> TryCatch(ReturningSalaryFucnction returningSalaryFunction)
+        {
+            try
+            {
+                return await returningSalaryFunction();
+            }
+            catch (NullSalaryException nullSalaryException)
+            {
+                throw CreateAndLogValidationException(nullSalaryException);
+            }
+        }
+
+        private SalaryValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var salaryValidationException = new SalaryValidationException(exception);
+            this.loggingBroker.LogError(salaryValidationException);
+
+            return salaryValidationException;
+        }
+    }
+}
---
+++ CashOverflow/Services/Foundations/Salaries/SalaryService.Validations.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Models.Salaries;
+using CashOverflow.Models.Salaries.Exceptions;
+
+namespace CashOverflow.Services.Foundations.Salaries
+{
+    public partial class SalaryService
+    {
+        private static void ValidateSalaryNotNull(Salary salary)
+        {
+            if (salary is null)
+            {
+                throw new NullSalaryException();
+            }
+        }
+    }
+}
