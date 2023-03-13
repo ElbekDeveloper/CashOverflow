9b8486fb4e465bd7c3dd771086d6babbcbfa2699
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-13 21:17:42 +0500

    CODE RUB: Fix Missing Code

--- CashOverflow/Startup.cs
+++ CashOverflow/Startup.cs
@@ -6,6 +6,8 @@
 using CashOverflow.Brokers.DateTimes;
 using CashOverflow.Brokers.Loggings;
 using CashOverflow.Brokers.Storages;
+using CashOverflow.Services.Foundations.Languages;
+using CashOverflow.Services.Foundations.Locations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -35,7 +37,7 @@ namespace CashOverflow
             });
 
             AddBrokers(services);
-            AddServices(services);
+            AddFoundationServices(services);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
