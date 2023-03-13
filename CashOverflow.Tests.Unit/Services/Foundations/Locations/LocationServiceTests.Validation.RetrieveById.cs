1c2c168a6776f6227ae39f20e1bf3e4a7d362a86
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-13 17:13:40 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> WIP

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Validation.RetrieveById.cs
@@ -0,0 +1,19 @@
+// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Locations
+{
+    public partial class LocationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            
+        }
+    }
+}
