6b2b17193f57005a4fe454875be0d6b1fe3fccfd
KholmatovKhabibulla <85988278+khabibulladev@users.noreply.github.com>
2023-03-13 18:49:41 +0500

    ShouldThrowValidationExceptionOnAddIfSalaryIsInvalidAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Salaries/SalaryServiceTests.Validations.Add.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Salaries/SalaryServiceTests.Validations.Add.cs
@@ -3,6 +3,7 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
+using System.ComponentModel;
 using System.Threading.Tasks;
 using CashOverflow.Models.Salaries;
 using CashOverflow.Models.Salaries.Exceptions;
@@ -41,6 +42,62 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Salaries
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfSalaryIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidSalary = new Salary
+            {
+                Amount = default(int)
+            };
+
+            var invalidSalaryException = new InvalidSalaryException();
+
+            invalidSalaryException.AddData(
+                key: nameof(Salary.Id),
+                values: "Id is required.");
+
+            invalidSalaryException.AddData(
+                key: nameof(Salary.Amount),
+                values: "Amount is required.");
+
+            invalidSalaryException.AddData(
+                key: nameof(Salary.Experience),
+                values: "Experience is required.");
+
+            invalidSalaryException.AddData(
+                key: nameof(Salary.CreatedDate),
+                values: "Date is required.");
+
+            var expectedSalaryValidationException =
+                new SalaryValidationException(invalidSalaryException);
+
+            // when
+            ValueTask<Salary> addSalaryTask =
+                this.salaryService.AddSalaryAsync(invalidSalary);
+
+            SalaryValidationException actualSalaryValidationException =
+                await Assert.ThrowsAsync<SalaryValidationException>(addSalaryTask.AsTask);
+
+            // then
+            actualSalaryValidationException.Should()
+                .BeEquivalentTo(expectedSalaryValidationException);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSalaryValidationException))), Times.Once());
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.InsertSalaryAsync(It.IsAny<Salary>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
