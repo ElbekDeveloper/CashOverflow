93af49ffe741ec6ef8579dea220c69eaf52befbb
KhusanRahmatullayev <125781453+KhusanRahmatullayev@users.noreply.github.com>
2023-03-15 01:44:55 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync ->WIP

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Locations
+{
+    public partial class LocationServiceTests
+    {
+
+    }
+}
