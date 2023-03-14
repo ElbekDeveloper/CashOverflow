c81195e730b9f7388dde7e0682252f573eda071a
Ulugbek <ulugbektoshtemirov30052000@gmail.com>
2023-03-14 11:59:35 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdAsyncIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exception.RetrieveById.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using CashOverflow.Models.Languages;
+using CashOverflow.Models.Languages.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdAsyncIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someId= Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedLanguageStorageException = 
+                new FailedLanguageStorageException(sqlException);
+
+            LanguageDependencyException expectedLanguageDependencyException =
+                new LanguageDependencyException(failedLanguageStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>())).ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Language> retrieveLanguageByIdTask=
+                this.languageService.RetrieveLanguageByIdAsync(someId);
+
+            LanguageDependencyException actualLanguageDependencyException =
+                await Assert.ThrowsAsync<LanguageDependencyException>(retrieveLanguageByIdTask.AsTask);
+
+            //then
+            actualLanguageDependencyException.Should().BeEquivalentTo(expectedLanguageDependencyException);
+
+            this.storageBrokerMock.Verify(broker=>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedLanguageDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+
+        }
+
+    }
+}
