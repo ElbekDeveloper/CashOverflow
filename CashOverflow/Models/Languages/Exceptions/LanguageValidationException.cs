62c10ee22de41c080ab2587eb3001c0d2882604b
Ulugbek <ulugbektoshtemirov30052000@gmail.com>
2023-03-15 17:27:20 +0500

    CODE RUB: Add Headerfile

--- CashOverflow/Models/Languages/Exceptions/LanguageValidationException.cs
+++ CashOverflow/Models/Languages/Exceptions/LanguageValidationException.cs
@@ -1,4 +1,9 @@
-﻿using Xeptions;
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Xeptions;
 
 namespace CashOverflow.Models.Languages.Exceptions
 {
