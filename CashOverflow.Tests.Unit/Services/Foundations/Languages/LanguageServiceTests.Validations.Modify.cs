bc4f3ddb5ae0908a0a39d9b1d02d69c5f2933fd0
KhusanRahmatullayev <125781453+KhusanRahmatullayev@users.noreply.github.com>
2023-03-16 00:51:24 +0500

    ShouldThrowValidationExceptionOnModifyIfLanguageIsNullAndLogItAsync ->WIP

--- CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Validations.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Validations.Modify.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CashOverflow.Models.Languages;
+using CashOverflow.Models.Languages.Exceptions;
+using Xunit;
 
 namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
 {
     public partial class LanguageServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfLanguageIsNullAndLogItAsync()
+        {
+            //given
+            Language nullLanguage= null;
+            var nullLanguageException = new NullLanguageException();
+
+            var excpectedLanguageValidationException =
+                new LanguageValidationException(nullLanguageException);
+
+            //when
+
+        }
     }
 }
