f5360380bd71e53c164db3ea025c88ba3871f96b
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-19 02:21:32 +0500

    ShouldThrowValidationExceptionOnModifyIfLocationIsInvalidAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Validations.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Validations.Modify.cs
@@ -6,6 +6,8 @@
 using System.Threading.Tasks;
 using CashOverflow.Models.Locations;
 using CashOverflow.Models.Locations.Exceptions;
+using FluentAssertions;
+using Force.DeepCloner;
 using Moq;
 using Xunit;
 
@@ -42,6 +44,69 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Locations
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfLocationIsInvalidAndLogItAsync(string invalidText)
+        {
+            //given
+            var invalidLocation = new Location
+            {
+                Name = invalidText
+            };
+
+            var invalidLocationException = new InvalidLocationException();
+
+            invalidLocationException.AddData(
+                key: nameof(Location.Id),
+                values: "Id is required");
+            
+            invalidLocationException.AddData(
+                key: nameof(Location.Name),
+                values: "Text is required");
+
+            invalidLocationException.AddData(
+                key: nameof(Location.CreatedDate),
+                values: "Value is required");
+            
+            invalidLocationException.AddData(
+                key: nameof(Location.UpdatedDate),
+                values: "Value is required");
+
+            var expectedLocationValidationException =
+                new LocationValidationException(invalidLocationException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset()).Returns(GetRandomDatetimeOffset);
+
+            //when
+            ValueTask<Location> modifyLocationTask =
+                this.locationService.ModifyLocationAsync(invalidLocation);
+
+            LocationValidationException actualLocationValidationException =
+                await Assert.ThrowsAsync<LocationValidationException>(modifyLocationTask.AsTask);
+
+            //then
+            actualLocationValidationException.Should().BeEquivalentTo(
+                expectedLocationValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedLocationValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateLocationAsync(It.IsAny<Location>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
