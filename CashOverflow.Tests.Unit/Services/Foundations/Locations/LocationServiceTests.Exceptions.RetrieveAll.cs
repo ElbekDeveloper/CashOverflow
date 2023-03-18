215e7eb0f7814101e1971bd2c413128995946493
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-03-18 13:31:51 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using CashOverflow.Models.Locations.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Locations
+{
+    public partial class LocationServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+
+            var failedStorageException =
+                new FailedLocationStorageException(sqlException);
+
+            var expectedLocationDependencyException =
+                new LocationDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllLocations())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllLocationsAction = () =>
+               this.locationService.RetrieveAllLocations();
+
+            LocationDependencyException actualLocationDependencyException =
+                Assert.Throws<LocationDependencyException>(
+                    retrieveAllLocationsAction);
+
+            // then
+            actualLocationDependencyException.Should().BeEquivalentTo(
+               expectedLocationDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllLocations(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedLocationDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
