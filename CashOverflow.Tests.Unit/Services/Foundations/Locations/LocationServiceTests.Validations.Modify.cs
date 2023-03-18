d02a1349a08a18b660214887175d5bfa94cf8611
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-19 01:41:22 +0500

    ShouldThrowValidationExceptionOnModifyIfLocationIsNullAndLogItAsync -> PASS

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Validations.Modify.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using CashOverflow.Models.Locations;
+using CashOverflow.Models.Locations.Exceptions;
+using Moq;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Locations
+{
+    public partial class LocationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfLocationIsNullAndLogItAsync()
+        {
+            //given
+            Location nullLocation = null;
+            var nullLocationException = new NullLocationException();
+
+            var expectedLocationValidationException =
+                new LocationValidationException(nullLocationException);
+
+            //when
+            ValueTask<Location> modifyLocationTask =
+                this.locationService.ModifyLocationAsync(nullLocation);
+
+            //then
+            await Assert.ThrowsAsync<LocationValidationException>(() => 
+                modifyLocationTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedLocationValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateLocationAsync(It.IsAny<Location>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+
+        }
+    }
+}
