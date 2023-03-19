114db00c2423c983f3ed200c33ff5f6325bf0104
KhusanRahmatullayev <125781453+KhusanRahmatullayev@users.noreply.github.com>
2023-03-19 10:11:04 +0500

    CODE RUB:Resolve Conflict

--- CashOverflow/Services/Foundations/Locations/LocationService.cs
+++ CashOverflow/Services/Foundations/Locations/LocationService.cs
@@ -4,6 +4,7 @@
 // --------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CashOverflow.Brokers.DateTimes;
 using CashOverflow.Brokers.Loggings;
