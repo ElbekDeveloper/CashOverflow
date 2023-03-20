d5f3b49ffbcd3c9c4d3ec04a477ac838a932b980
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-03-20 16:53:05 +0500

    ShouldThrowValidationExceptionOnModifyIfJobIsNullAndLogItAsync -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Jobs/JobServiceTests.Validations.Modify.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using CashOverflow.Models.Jobs;
+using CashOverflow.Models.Jobs.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Jobs
+{
+    public partial class JobServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfJobIsNullAndLogItAsync()
+        {
+            // given
+            Job nullJob = null;
+            var nullJobException = new NullJobException();
+            
+            var expectedJobValidationException =
+                new JobValidationException(nullJobException);
+
+            // when
+            ValueTask<Job> modifyJobTask = this.jobService.ModifyJobAsync(nullJob);
+
+            JobValidationException actualJobValidationException =
+                await Assert.ThrowsAsync<JobValidationException>(
+                    modifyJobTask.AsTask);
+
+            // then
+            actualJobValidationException.Should().BeEquivalentTo(expectedJobValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedJobValidationException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ CashOverflow/Models/Jobs/Exceptions/NullJobException.cs
@@ -0,0 +1,10 @@
+﻿using Xeptions;
+
+namespace CashOverflow.Models.Jobs.Exceptions
+{
+    public class NullJobException : Xeption
+    {
+        public NullJobException() : base(message: "Job is null")
+        { }
+    }
+}
