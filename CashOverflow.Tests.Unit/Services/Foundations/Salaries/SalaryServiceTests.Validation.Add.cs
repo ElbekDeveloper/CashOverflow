616ecb1c0aef2aa1d1b94db757dc1fde39cf606f
KholmatovKhabibulla <85988278+khabibulladev@users.noreply.github.com>
2023-03-20 09:59:13 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> WIP

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Salaries/SalaryServiceTests.Validation.Add.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Salaries
+{
+    public class SalaryServiceTests
+    {
+    }
+}
