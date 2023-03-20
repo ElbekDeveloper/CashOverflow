5cc16d1ff5ea357cbc781a42ca8f1f8e7d072c0a
jahongir1507 <ishankulovjahongir1507@gmail.com>
2023-03-20 21:41:38 +0500

    DATA: Model & Migrate `Review`

---
+++ CashOverflow/Models/Reviews/Review.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+namespace CashOverflow.Models.Reviews
+{
+    public class Review
+    {
+        public string CompanyName { get; set; }
+        public int Stars { get; set; }
+        public string Thoughts { get; set; }
+    }
+}
