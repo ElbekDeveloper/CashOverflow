734fd680a606f5d7752016f1b46f1eb830ea58b8
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-03-21 00:58:31 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfDepdencyErrorOccursAndLogItAsync -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Salaries/SalaryServiceTests.Exceptions.Add.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Models.Salaries.Exceptions;
+using CashOverflow.Models.Salaries;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Salaries
+{
+    public partial class SalaryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfDepdencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Salary someSalary = CreateRandomSalary();
+            SqlException sqlException = CreateSqlException();
+            var faildSalaryStorageException = new FailedSalaryStorageException(sqlException);
+            var expectedSalaryDependencyException = new SalaryDependencyException(faildSalaryStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker => broker.GetCurrentDateTimeOffset())
+               .Throws(sqlException);
+
+            // when
+            ValueTask<Salary> addSalaryTask = this.salaryService.AddSalaryAsync(someSalary);
+
+            SalaryDependencyException actualLocationDependencyException =
+                await Assert.ThrowsAsync<SalaryDependencyException>(addSalaryTask.AsTask);
+
+            // then
+            actualLocationDependencyException.Should().BeEquivalentTo(expectedSalaryDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+            broker.GetCurrentDateTimeOffset(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => broker.LogCritical(It.Is(SameExceptionAs(expectedSalaryDependencyException))), Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
