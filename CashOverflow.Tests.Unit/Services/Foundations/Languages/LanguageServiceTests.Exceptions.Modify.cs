de34debced743fe5cbc8b0312213e4ee6f944d7b
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-21 17:21:23 +0500

    CODE RUP: Clean Up & Add Header Text

--- CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.Modify.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
 using System.Threading.Tasks;
 using CashOverflow.Models.Languages;
 using CashOverflow.Models.Languages.Exceptions;
