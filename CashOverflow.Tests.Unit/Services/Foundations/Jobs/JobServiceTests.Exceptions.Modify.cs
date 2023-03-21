d00cd56ca603e59d3af589858496df861f42dfdf
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2023-03-21 22:35:25 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Jobs/JobServiceTests.Exceptions.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Jobs/JobServiceTests.Exceptions.Modify.cs
@@ -7,8 +7,10 @@ using System;
 using System.Threading.Tasks;
 using CashOverflow.Models.Jobs;
 using CashOverflow.Models.Jobs.Exceptions;
+using Castle.Components.DictionaryAdapter.Xml;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -63,6 +65,59 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Jobs
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            int minutesInPast = GetRandomNegativeNumber();
+            DateTimeOffset randomDateTime = GetRandomDatetimeOffset();
+            Job randomJob = CreateRandomJob(randomDateTime);
+            Job someJob = randomJob;
+            Guid jobId = someJob.Id;
+            someJob.CreatedDate = someJob.CreatedDate.AddMinutes(minutesInPast);
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedStorageJobException =
+                new FailedJobStorageException(databaseUpdateException);
+
+            var expectedJobDependencyException =
+                new JobDependencyException(databaseUpdateException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectJobByIdAsync(jobId))
+                    .ThrowsAsync(databaseUpdateException);
+
+            this.dateTimeBrokerMock.Setup(broker => 
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTime);
+
+            // when
+            ValueTask<Job> modifyJobTask = 
+                this.jobService.ModifyJobAsync(someJob);
+
+            JobDependencyException actualJobDependencyException =
+                await Assert.ThrowsAsync<JobDependencyException>(
+                    modifyJobTask.AsTask);
+
+            // then
+            actualJobDependencyException.Should().BeEquivalentTo(expectedJobDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectJobByIdAsync(jobId), Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedJobDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
         }
     }
 }
