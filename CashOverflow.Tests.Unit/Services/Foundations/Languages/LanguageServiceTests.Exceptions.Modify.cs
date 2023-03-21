c1799d1285157e91648752ef5ff2c0e806429ec5
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-21 14:41:20 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.Modify.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CashOverflow.Models.Languages;
+using CashOverflow.Models.Languages.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
 using Xunit;
 
 namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
@@ -12,7 +17,50 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
         [Fact]
         public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
         {
-            throw new NotImplementedException();
+            //given
+            DateTimeOffset someDateTime = GetRandomDatetimeOffset();
+            Language randomLanguage = CreateRandomLanguage(someDateTime);
+            Language someLanguage = randomLanguage;
+            Guid languageId = someLanguage.Id;
+            SqlException sqlException = CreateSqlException();
+
+            var failedLanguageStorageException = 
+                new FailedLanguageStorageException(sqlException);
+            
+            var expectedLanguageDependencyException = 
+                new LanguageDependencyException(failedLanguageStorageException);
+            
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset()).Throws(sqlException);
+            
+            //when
+            ValueTask<Language> modifyLanguageTask = 
+                this.languageService.ModifyLanguageAsync(someLanguage);
+            
+            LanguageDependencyException actualLanguageDependencyException =
+                await Assert.ThrowsAsync<LanguageDependencyException>(
+                    modifyLanguageTask.AsTask);
+            
+            //then
+            actualLanguageDependencyException.Should().BeEquivalentTo(
+                    expectedLanguageDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedLanguageDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectLanguageByIdAsync(languageId), Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateLanguageAsync(someLanguage), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();  
         }
     }
 }
