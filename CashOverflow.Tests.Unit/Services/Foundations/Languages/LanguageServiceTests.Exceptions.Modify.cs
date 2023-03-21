3208f768a1004bc3f7b3d52cc111b5971d4db33b
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-21 14:04:43 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> WIP

--- CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.Modify.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
 {
     public partial class LanguageServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
