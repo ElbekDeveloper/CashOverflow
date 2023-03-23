effc46bf8b3485ea9d1309ee800f8ef1776e5d48
bilol.kobilov <bilol.kobilov@s.amity.edu>
2023-03-24 00:22:09 +0500

    BROKERS: Select All Reviews

--- CashOverflow/Brokers/Storages/IStorageBroker.Reviews.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Reviews.cs
@@ -3,8 +3,13 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
+using System.Linq;
+using CashOverflow.Models.Reviews;
+
 namespace CashOverflow.Brokers.Storages
 {
     public partial interface IStorageBroker
-    { }
+    {
+        IQueryable<Review> SelectAllReviews();
+    }
 }
