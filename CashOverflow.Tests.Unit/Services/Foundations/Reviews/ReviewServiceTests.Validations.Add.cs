5d06505c2ce63590f8391523e39bb48308fa1caf
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-25 17:13:27 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Reviews/ReviewServiceTests.Validations.Add.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Reviews/ReviewServiceTests.Validations.Add.cs
@@ -6,20 +6,46 @@
 using System;
 using System.Threading.Tasks;
 using CashOverflow.Models.Reviews;
+using CashOverflow.Models.Reviews.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
 
 namespace CashOverflow.Tests.Unit.Services.Foundations.Reviews
 {
-	public partial class ReviewServiceTests
-	{
+    public partial class ReviewServiceTests
+    {
+        [Fact]
         public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
         {
-           // given
+            // given
+            Review nullReview = null;
+            var nullReviewException = new NullReviewException();
 
+            var expectedReviewValidationException =
+                new ReviewValidationException(nullReviewException);
 
-           // when
+            // when 
+            ValueTask<Review> addReviewTask =
+                this.reviewService.AddReviewAsync(nullReview);
 
+            ReviewValidationException actualReviewValidationException =
+                await Assert.ThrowsAsync<ReviewValidationException>(addReviewTask.AsTask);
 
             // then
+            actualReviewValidationException.Should()
+                .BeEquivalentTo(expectedReviewValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedReviewValidationException)))
+                    , Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertReviewAsync(It.IsAny<Review>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
