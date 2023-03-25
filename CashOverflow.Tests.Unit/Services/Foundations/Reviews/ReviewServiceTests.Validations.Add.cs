d22387789393e68c1bee2dea3bf1caab8d4a6e4d
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-25 16:21:33 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> WIP

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Reviews/ReviewServiceTests.Validations.Add.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CashOverflow.Models.Reviews;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Reviews
+{
+	public partial class ReviewServiceTests
+	{
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+           // given
+
+
+           // when
+
+
+            // then
+        }
+    }
+}
+
---
+++ CashOverflow/Models/Reviews/Exceptions/NullReviewException.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace CashOverflow.Models.Reviews.Exceptions
+{
+	public class NullReviewException:Xeption
+	{
+		public NullReviewException()
+			: base(message: "Review is null.")
+		{ }
+	}
+}
+
