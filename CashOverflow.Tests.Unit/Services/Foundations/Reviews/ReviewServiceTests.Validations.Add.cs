4105865e997863c1854a5f33a256fbee6ecace65
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-25 17:52:35 +0500

    ShouldThrowValidationExceptionOnAddIfReviewIsInvalidAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Reviews/ReviewServiceTests.Validations.Add.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Reviews/ReviewServiceTests.Validations.Add.cs
@@ -7,6 +7,8 @@ using System;
 using System.Threading.Tasks;
 using CashOverflow.Models.Reviews;
 using CashOverflow.Models.Reviews.Exceptions;
+using CashOverflow.Models.Reviews;
+using CashOverflow.Models.Reviews.Exceptions;
 using FluentAssertions;
 using Moq;
 using Xunit;
@@ -46,6 +48,62 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Reviews
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfReviewIsInvalidAndLogItAsync(
+    string invalidText)
+        {
+            // given
+            Review invalidReview = new Review()
+            {
+                CompanyName = invalidText,
+                Thoughts = invalidText
+            };
+
+            var invalidReviewException = new InvalidReviewException();
+
+            invalidReviewException.AddData(
+                key: nameof(Review.Id),
+                values: "Id is required");
+
+            invalidReviewException.AddData(
+                key: nameof(Review.CompanyName),
+                values: "Company name is required");
+
+            invalidReviewException.AddData(
+                key: nameof(Review.Stars),
+                values: "Stars are required");
+
+            invalidReviewException.AddData(
+                key: nameof(Review.Thoughts),
+                values: "Thoughts are required");
+
+            var expectedReviewValidationException =
+                new ReviewValidationException(invalidReviewException);
+
+            // when
+            ValueTask<Review> addReviewTask = this.reviewService.AddReviewAsync(invalidReview);
+
+            ReviewValidationException actualReviewValidationException =
+                await Assert.ThrowsAsync<ReviewValidationException>(addReviewTask.AsTask);
+
+            // then
+            actualReviewValidationException.Should()
+                .BeEquivalentTo(expectedReviewValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedReviewValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertReviewAsync(It.IsAny<Review>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
