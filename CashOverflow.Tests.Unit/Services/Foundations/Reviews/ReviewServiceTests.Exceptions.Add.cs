50128d4c3b278eaf47270a1d6a6b28ba168520b1
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-26 18:33:46 +0500

    ShoudlThrowCriticalDependencyExceptionOnAddIfDependencyErrorOccursAndLogItAsync -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Reviews/ReviewServiceTests.Exceptions.Add.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using CashOverflow.Models.Reviews;
+using CashOverflow.Models.Reviews.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Reviews
+{
+    public partial class ReviewServiceTests
+    {
+
+        [Fact]
+        public async Task ShoudlThrowCriticalDependencyExceptionOnAddIfDependencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Review someReview = CreateRandomReview();
+            SqlException sqlException = CreateSqlException();
+            var failedReviewStorageException = new FailedReviewStorageException(sqlException);
+            var expectedReviewDependencyException = new ReviewDependencyException(failedReviewStorageException);
+
+            // when
+            ValueTask<Review> addReviewTask = this.reviewService.AddReviewAsync(someReview);
+
+            ReviewDependencyException actualReviewDependencyException =
+                await Assert.ThrowsAsync<ReviewDependencyException>(addReviewTask.AsTask);
+
+            // then
+            actualReviewDependencyException.Should().BeEquivalentTo(expectedReviewDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(
+                    SameExceptionAs(expectedReviewDependencyException))), Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
+
