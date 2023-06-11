23b40b25ba6cf94c0764dd3e74e5def24e90c6be
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-06-11 15:15:57 +0500

    DATA: Model & Migrate `Companies`

---
+++ CashOverflow/Brokers/Storages/StorageBroker.Companies.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+using CashOverflow.Models.Companies;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashOverflow.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ CashOverflow/Migrations/20230611095850_CreateCompaniesTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashOverflow.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCompaniesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
