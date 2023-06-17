a4bf4eb67e999386113c7e2329a400ab63aa55ff
Dilshod Abdijabbarov <94256812+Dilshod-Abdijabbarov@users.noreply.github.com>
2023-06-17 15:25:31 +0500

    BROKERS: Update Company

---
+++ CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
@@ -0,0 +1,10 @@
+﻿using CashOverflow.Models.Companies;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Company> UpdateCompanyAsync(Company company);
+    }
+}
--- CashOverflow/Brokers/Storages/StorageBroker.Companies.cs
+++ CashOverflow/Brokers/Storages/StorageBroker.Companies.cs
@@ -5,11 +5,15 @@
 
 using CashOverflow.Models.Companies;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace CashOverflow.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Company> Companies { get; set; }
+
+        public async ValueTask<Company> UpdateCompanyAsync(Company company) =>
+            await UpdateAsync(company);
     }
 }
