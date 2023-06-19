9319d220b23785f442b8d48727a7a682daf816cd
devdoston <dostonbekkurbanov1@gmail.com>
2023-06-19 11:09:23 +0500

    BROKERS: Delete Company #202

--- CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
@@ -11,5 +11,6 @@ namespace CashOverflow.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Company> InsertCompanyAsync(Company company);
+        ValueTask<Company> DeleteCompanyAsync(Company company);
     }
 }
