bcf2c503566f8efa4818d5a6d1d34001ed4b560e
Javlonbek <fayzullaevjavlonbek.d@gmail.com>
2023-06-20 22:32:59 +0500

    CODE RUB: Added Company interface and service in StorageBroker

--- CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
@@ -3,6 +3,7 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using CashOverflow.Models.Companies;
 
@@ -10,6 +11,7 @@ namespace CashOverflow.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        IQueryable<Company> SelectAllCompanies();
         ValueTask<Company> InsertCompanyAsync(Company company);
         ValueTask<Company> DeleteCompanyAsync(Company company);
     }
