599fc22bd813876c7863a2f239d5b43019e0e5bd
Javlonbek <fayzullaevjavlonbek.d@gmail.com>
2023-06-21 19:33:08 +0500

    Added SelectAllCompanies in StrageBroker

--- CashOverflow/Brokers/Storages/StorageBroker.Companies.cs
+++ CashOverflow/Brokers/Storages/StorageBroker.Companies.cs
@@ -13,14 +13,11 @@ namespace CashOverflow.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Company> Companies { get; set; }
-
-        
-        public IQueryable<Company> SelectAllCompanies() =>
-            SelectAll<Company>();
-            
+             
         public async ValueTask<Company> InsertCompanyAsync(Company company) =>
             await InsertAsync(company);
-
+        public IQueryable<Company> SelectAllCompanies() =>
+            SelectAll<Company>(); 
         public async ValueTask<Company> DeleteCompanyAsync(Company company) =>
             await DeleteAsync(company);
     }
