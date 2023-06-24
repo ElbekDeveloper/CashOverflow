f73a0d00694eed4cf71cce85b9a7c0ecbbd607df
ElbekDeveloper <normurodovelbek0133@gmail.com>
2023-06-24 22:49:02 +0500

    ShouldThrowValidationExceptionOnAddIfCompanyIsInvalidAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Validations.Add.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Validations.Add.cs
@@ -3,6 +3,8 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
+using System;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using CashOverflow.Models.Companies;
 using CashOverflow.Models.Companies.Exceptions;
@@ -45,5 +47,57 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Companies
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfCompanyIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidCompany = new Company
+            {
+                Name = invalidText
+            };
+
+            var invalidCompanyException = new InvalidCompanyException();
+
+            invalidCompanyException.AddData(
+                key: nameof(Company.Id),
+                values: "Id is required");
+
+            invalidCompanyException.AddData(
+                key: nameof(Company.Name),
+                values: "Text is required");
+
+            invalidCompanyException.AddData(
+                key: nameof(Company.CreatedDate),
+                values: "Value is required");
+
+            var expectedCompanyValidationException =
+                new CompanyValidationException(invalidCompanyException);
+
+            // when
+            ValueTask<Company> addCompanyTask = 
+                this.companyService.AddCompanyAsync(invalidCompany);
+
+            CompanyValidationException actualCompanyValidationException =
+                await Assert.ThrowsAsync<CompanyValidationException>(addCompanyTask.AsTask);
+
+            // then
+            actualCompanyValidationException.Should().BeEquivalentTo(
+                expectedCompanyValidationException);
+
+            this.loggingBrokerMock.Verify(broker => broker.LogError(
+                It.Is(SameExceptionAs(expectedCompanyValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker => broker.InsertCompanyAsync(
+                It.IsAny<Company>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
