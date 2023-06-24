b6b2592dac71c18373d6ec7aff866cedfce60283
Max <sultonbek.rakhimov@gmail.com>
2023-06-24 21:01:12 +0500

    BROKERS : Select Company By Id

--- CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
+++ CashOverflow/Brokers/Storages/IStorageBroker.Companies.cs
@@ -3,6 +3,7 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using CashOverflow.Models.Companies;
 
@@ -11,6 +12,7 @@ namespace CashOverflow.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Company> InsertCompanyAsync(Company company);
+        ValueTask<Company> SelectCompanyByIdAsync(Guid companyId);
         ValueTask<Company> DeleteCompanyAsync(Company company);
         ValueTask<Company> UpdateCompanyAsync(Company company);
     }
