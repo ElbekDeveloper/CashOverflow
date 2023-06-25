e077663dd9ace4ce8fb6f667b2dbaeb7b21e102d
Asrorjon Kodirov <asrorjon.kodirov@techcells.com>
2023-06-25 14:32:56 +0500

    ShouldModifyCompanyAsync -> FAIL

---
+++ CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Logic.Modify.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using CashOverflow.Models.Companies;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Companies
+{
+    public partial class CompanyServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyCompanyAsync()
+        {
+            // given
+            DateTimeOffset randomDate = GetRandomDateTime();
+            Company randomCompany = CreateRandomModifyCompany(randomDate);
+            Company inputCompany = randomCompany;
+            Company storageCompany = inputCompany.DeepClone();
+            Company updatedCompany = inputCompany;
+            Company expectedCompany = updatedCompany.DeepClone();
+            Guid companyId = inputCompany.Id;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateCompanyAsync(inputCompany))
+                    .ReturnsAsync(updatedCompany);
+
+            // when
+            Company actualCompany =
+               await this.companyService.ModifyCompanyAsync(inputCompany);
+
+            // then
+            actualCompany.Should().BeEquivalentTo(expectedCompany);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCompanyAsync(inputCompany), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
