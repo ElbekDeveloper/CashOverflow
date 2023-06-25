536b8c578ff737d413b1030b89aefe825f51cb39
Asrorjon Kodirov <asrorjon.kodirov@techcells.com>
2023-06-25 21:55:36 +0500

    ShouldThrowValidationExceptionOnModifyIfCompanyIsNullAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Validations.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Validations.Modify.cs
@@ -41,5 +41,61 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Companies
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfCompanyIsInvalidAndLogItAsync(string invalidString)
+        {
+            // given
+            var invalidCompany = new Company
+            {
+                Name = invalidString
+            };
+
+            var invalidCompanyException = new InvalidCompanyException();
+
+            invalidCompanyException.AddData(
+            key: nameof(Company.Id),
+                values: "Id is required");
+
+            invalidCompanyException.AddData(
+                key: nameof(Company.Name),
+                values: "Text is required");
+
+            invalidCompanyException.AddData(
+                key: nameof(Company.CreatedDate),
+                values: "Date is required");
+
+            var expectedCompanyValidationException = 
+                new CompanyValidationException(invalidCompanyException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(GetRandomDateTime);
+
+            // when
+            ValueTask<Company> modifyCompanyTask = 
+                this.companyService.ModifyCompanyAsync(invalidCompany);
+
+            CompanyValidationException actualCompanyValidationException =
+                await Assert.ThrowsAsync<CompanyValidationException>(
+                    modifyCompanyTask.AsTask);
+
+            // then
+            actualCompanyValidationException.Should().BeEquivalentTo(expectedCompanyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCompanyValidationException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ CashOverflow/Models/Companies/Exceptions/InvalidCompanyException.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Xeptions;
+
+namespace CashOverflow.Models.Companies.Exceptions
+{
+    public class InvalidCompanyException : Xeption
+    {
+        public InvalidCompanyException()
+            : base(message: "Company is invalid.")
+        { }
+    }
+}
