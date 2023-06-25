7650c6a59f86a762d26e38337f618bc71bf3c9dc
ElbekDeveloper <normurodovelbek0133@gmail.com>
2023-06-25 12:00:07 +0500

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotRecentAndLogItAsync -> PASS

--- CashOverflow/Services/Foundations/Companies/CompanyService.Validations.cs
+++ CashOverflow/Services/Foundations/Companies/CompanyService.Validations.cs
@@ -3,24 +3,25 @@
 // Developed by CashOverflow Team
 // --------------------------------------------------------
 
-using CashOverflow.Models.Companies.Exceptions;
-using CashOverflow.Models.Companies;
 using System;
-using System.Data;
+using System.Transactions;
+using CashOverflow.Models.Companies;
+using CashOverflow.Models.Companies.Exceptions;
 
 namespace CashOverflow.Services.Foundations.Companies
 {
     public partial class CompanyService
     {
 
-        private static void ValidateCompany(Company company)
+        private void ValidateCompany(Company company)
         {
             ValidateCompanyNotNull(company);
 
             Validate(
                 (Rule: IsInvalid(company.Id), Parameter: nameof(Company.Id)),
                 (Rule: IsInvalid(company.Name), Parameter: nameof(Company.Name)),
-                (Rule: IsInvalid(company.CreatedDate), Parameter: nameof(Company.CreatedDate)));
+                (Rule: IsInvalid(company.CreatedDate), Parameter: nameof(Company.CreatedDate)),
+                (Rule: IsNotRecent(company.CreatedDate), Parameter: nameof(Company.CreatedDate)));
         }
 
         private static void ValidateCompanyNotNull(Company company)
@@ -48,6 +49,20 @@ namespace CashOverflow.Services.Foundations.Companies
             Condition = date == default,
             Message = "Value is required"
         };
+
+        private dynamic IsNotRecent(DateTimeOffset date) => new
+        {
+            Condition = IsDateNotRecent(date),
+            Message = "Value is not recent"
+        };
+
+        private bool IsDateNotRecent(DateTimeOffset date)
+        {
+            DateTimeOffset now = this.dateTimeBroker.GetCurrentDateTimeOffset();
+            TimeSpan timeDifference = now.Subtract(date);
+
+            return timeDifference.TotalSeconds is > 60 or < 0;
+        }
 
         private static void Validate(params (dynamic Rule, string Parameter)[] validations)
         {
