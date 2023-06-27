7713f14deffcfa7728ce40c4389404747c2137bc
Max <sultonbek.rakhimov@gmail.com>
2023-06-27 22:11:30 +0500

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyExceptionOccursAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Exceptions.Modify.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Exceptions.Modify.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------
+// --------------------------------------------------------
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Developed by CashOverflow Team
 // --------------------------------------------------------
@@ -99,7 +99,49 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Companies
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();;
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyExceptionOccursAndLogItAsync()
+        {
+            // given 
+            Company randomCompany = CreateRandomCompany();
+            Company someCompany = randomCompany;
+            Guid companyId = someCompany.Id;
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var failedCompanyStorageException = 
+                new FailedCompanyStorageException(databaseUpdateConcurrencyException);
+
+            var expectedCompanyDependencyException =
+                new CompanyDependencyException(failedCompanyStorageException);
+
+            this.storageBrokerMock.Setup(broker => 
+                    broker.SelectCompanyByIdAsync(companyId)).ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when 
+            ValueTask<Company> modifyCompanyTask = 
+                this.companyService.ModifyCompanyAsync(someCompany);
+
+            CompanyDependencyException actualCompanyDependencyException =
+                await Assert.ThrowsAsync<CompanyDependencyException>(modifyCompanyTask.AsTask);
+
+            // then
+            actualCompanyDependencyException.Should()
+                .BeEquivalentTo(expectedCompanyDependencyException);
+            
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectCompanyByIdAsync(companyId), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedCompanyDependencyException))));
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
     }
