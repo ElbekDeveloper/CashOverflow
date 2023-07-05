175c817cb651a742c5aee9c67cbeba3c4220889a
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2023-07-06 02:17:55 +0500

    DATA: Upgrade & Migrate Review

---
+++ CashOverflow/Migrations/20230705204001_RemoveCompanyNameAndAddCompanyIdToReviewTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashOverflow.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCompanyNameAndAddCompanyIdToReviewTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CompanyId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
