d5e77cfc7182551a9d299f6dce77d0b7912701ef
devdoston <dostonbekkurbanov1@gmail.com>
2023-07-06 19:27:24 +0500

    DOCUMENTATIONS: Exception For Add Method

--- CashOverflow/Services/Foundations/Languages/ILanguageService.cs
+++ CashOverflow/Services/Foundations/Languages/ILanguageService.cs
@@ -12,7 +12,12 @@ namespace CashOverflow.Services.Foundations.Languages
 {
     public interface ILanguageService
     {
+        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageValidationException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyValidationException"></exception>
         ValueTask<Language> AddLanguageAsync(Language language);
+
         IQueryable<Language> RetrieveAllLanguages();
         ValueTask<Language> RetrieveLanguageByIdAsync(Guid languageId);
         ValueTask<Language> ModifyLanguageAsync(Language language);
