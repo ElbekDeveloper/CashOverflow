8145df68fc6d83938388affa41cb4073e9e392e8
Dostonbek Kurbanov <97718607+devdoston@users.noreply.github.com>
2023-07-06 22:58:43 +0500

    CODE RUB: Change Order Of Lines

--- CashOverflow/Services/Foundations/Languages/ILanguageService.cs
+++ CashOverflow/Services/Foundations/Languages/ILanguageService.cs
@@ -12,28 +12,28 @@ namespace CashOverflow.Services.Foundations.Languages
 {
     public interface ILanguageService
     {
-        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
-        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageValidationException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageDependencyValidationException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         ValueTask<Language> AddLanguageAsync(Language language);
 
-        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         IQueryable<Language> RetrieveAllLanguages();
 
-        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         ValueTask<Language> RetrieveLanguageByIdAsync(Guid languageId);
 
-        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageValidationException"></exception>
-        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageDependencyValidationException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         ValueTask<Language> ModifyLanguageAsync(Language language);
 
-        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageDependencyValidationException"></exception>
+        /// <exception cref="Models.Languages.Exceptions.LanguageDependencyException"></exception>
         /// <exception cref="Models.Languages.Exceptions.LanguageServiceException"></exception>
         ValueTask<Language> RemoveLanguageByIdAsync(Guid languageId);
     }
