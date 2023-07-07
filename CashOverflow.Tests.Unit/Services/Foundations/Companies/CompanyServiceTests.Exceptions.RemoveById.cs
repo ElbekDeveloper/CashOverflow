4af5732574f47cbe126f12340c88c176991720ec
Nuriddin <isoqov0910@gmail.com>
2023-07-07 16:53:34 +0500

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> FAIL

--- CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Exceptions.RemoveById.cs
+++ CashOverflow.Tests.Unit/Services/Foundations/Companies/CompanyServiceTests.Exceptions.RemoveById.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using CashOverflow.Models.Companies;
 using CashOverflow.Models.Companies.Exceptions;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
@@ -54,6 +55,45 @@ namespace CashOverflow.Tests.Unit.Services.Foundations.Companies
 
             this.storageBrokerMock.Verify(broker =>
                 broker.DeleteCompanyAsync(It.IsAny<Company>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedCompanyStorageException =
+                new FailedCompanyStorageException(sqlException);
+
+            var expectedCompanyDependencyException =
+                new CompanyDependencyException(failedCompanyStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCompanyByIdAsync(someId))
+                    .Throws(sqlException);
+            // when
+            ValueTask<Company> removeCompanyTask =
+                this.companyService.RemoveCompanyById(someId);
+
+            CompanyDependencyException actualCompanyDependencyException =
+                await Assert.ThrowsAsync<CompanyDependencyException>(
+                    removeCompanyTask.AsTask);
+
+            // then
+            actualCompanyDependencyException.Should().BeEquivalentTo(expectedCompanyDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCompanyByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCompanyDependencyException))), Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
