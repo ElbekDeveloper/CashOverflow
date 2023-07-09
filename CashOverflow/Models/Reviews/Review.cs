a0d97f33f0906d1a8f8037dfaedd222edaf957bb
ElbekDeveloper <normurodovelbek0133@gmail.com>
2023-07-09 11:39:35 +0500

    MINOR FIX: Add Foreign Key

--- CashOverflow/Models/Reviews/Review.cs
+++ CashOverflow/Models/Reviews/Review.cs
@@ -4,14 +4,17 @@
 // --------------------------------------------------------
 
 using System;
+using CashOverflow.Models.Companies;
 
 namespace CashOverflow.Models.Reviews
 {
     public class Review
     {
         public Guid Id { get; set; }
-        public Guid CompanyId { get; set; }
         public int Stars { get; set; }
         public string Thoughts { get; set; }
+
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
     }
 }
